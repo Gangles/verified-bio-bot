5d5242012fcb1ca4e8945dfba74c1da8c64fa9ce
Matthew Gallant <matthew@gangles.ca>
2015-01-21 08:06:00 -0800

    Stricter conditions to avoid posting phone numbers

--- verifiedbiobot.py
+++ verifiedbiobot.py
@@ -33,7 +33,7 @@ def clean_description(description):
     words = description.split()
     link = re.compile('https?:', re.IGNORECASE)
     email = re.compile('[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}', re.IGNORECASE)
-    phone = re.compile('[\d-]{7,}')
+    phone = re.compile('[\d-]{4,}')
     hashtags = re.compile('[@#]')
     for i, word in enumerate(words):
         if re.search(link, word):
